85a16fb4a09fc6eb52727dba87fe21c484d96913
haythamdahri <haytham.dahri@gmail.com>
2019-06-22 01:52:02 +0100

    Edit archive and archive members

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,9 +1,27 @@
+
+function confirmArchiveDelete(archiveId) {
+    Swal.fire({
+        title: "Êtes-vous sûre de supprimer l'archive?",
+        text: "Attention",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: '<i class=\'far fa-window-close\'></i> Annuler',
+        confirmButtonText: '<i class=\'far fa-check-circle\'></i> Oui, Supprimer!'
+    }).then((result) => {
+        if (result.value) {
+            $("#archive" + archiveId).submit();
+        }
+    })
+}
+
+
 $(document).ready(() => {
 
     $('.datepicker').datepicker({
         format: 'yyyy-mm-dd'
     });
-
 
 
 });
